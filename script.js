b65ca10107b1173815f82cceb299d339d75bb632
Kayke Araujo Guimarães <90934419+K4Z1NH0@users.noreply.github.com>
2024-02-23 21:24:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+function mostrarTexto(p1) {
+    var pElement1 = document.getElementById("p1");
+    var btnPlus1 = document.getElementById("btnPlus1");
+    var btnMinus1 = document.getElementById("btnMinus1");
+
+    if (pElement1.style.display === 'block') {
+        pElement1.style.display = 'none';
+        btnMinus1.style.display = 'none';
+        btnPlus1.style.display = 'block'
+    } else {
+        pElement1.style.display = 'block';
+        btnMinus1.style.display = 'block';
+        btnPlus1.style.display = 'none';
+    }
+}
+
+function mostrarTexto2(p2) {
+    var pElement2 = document.getElementById("p2");
+    var btnPlus2 = document.getElementById("btnPlus2");
+    var btnMinus2 = document.getElementById("btnMinus2");
+
+    if (pElement2.style.display === 'block') {
+        pElement2.style.display = 'none';
+        btnMinus2.style.display = 'none';
+        btnPlus2.style.display = 'block'
+    } else {
+        pElement2.style.display = 'block';
+        btnMinus2.style.display = 'block';
+        btnPlus2.style.display = 'none';
+    }
+}
+
+function mostrarTexto3(p3) {
+    var pElement3 = document.getElementById("p3");
+    var btnPlus3 = document.getElementById("btnPlus3");
+    var btnMinus3 = document.getElementById("btnMinus3");
+
+    if (pElement3.style.display === 'block') {
+        pElement3.style.display = 'none';
+        btnMinus3.style.display = 'none';
+        btnPlus3.style.display = 'block'
+    } else {
+        pElement3.style.display = 'block';
+        btnMinus3.style.display = 'block';
+        btnPlus3.style.display = 'none';
+    }
+}
+
+function mostrarTexto4(p4) {
+    var pElement4 = document.getElementById("p4");
+    var btnPlus4 = document.getElementById("btnPlus4");
+    var btnMinus4 = document.getElementById("btnMinus4");
+
+    if (pElement4.style.display === 'block') {
+        pElement4.style.display = 'none';
+        btnMinus4.style.display = 'none';
+        btnPlus4.style.display = 'block'
+    } else {
+        pElement4.style.display = 'block';
+        btnMinus4.style.display = 'block';
+        btnPlus4.style.display = 'none';
+    }
+}
